e399ed922d0cd7703a99c894af7ad7e8d0ab6e1d
Sreya Guha <sreyaguha@Ramanathans-MacBook-Pro.local>
2017-09-29 12:04:04 -0700

    trying with Xcode 9

---
+++ StackViewExample/example.swift
@@ -0,0 +1,13 @@
+//
+//  example.swift
+//  StackViewExample
+//
+//  Created by Sreya Guha on 9/29/17.
+//  Copyright © 2017 Sreya Guha. All rights reserved.
+//
+
+import Foundation
+
+func hello() {
+    print ("hello")
+}
